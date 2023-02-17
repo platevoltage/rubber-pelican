cb68fb7465e5dc8eaf416bfdc62f258c2c284611
antieatingactivist <antieatingactivist@gmail.com>
2023-02-16 17:45:07 -0800

    keyboard emulation working

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -1,9 +1,22 @@
 #include <Arduino.h>
+#include "hidkeyboard.h"
+
+// #if CFG_TUD_HID
+// #define KEYBOARD_I2C_ADDR     0X5f
+// #endif
+
+
+HIDkeyboard keyboard;
 
 void setup() {
   // put your setup code here, to run once:
+  keyboard.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  keyboard.sendChar('a');
+  delay(1000);
 }
+
+
