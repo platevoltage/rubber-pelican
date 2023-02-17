267363210ae53361f8dcbfbb7f4493aca5025834
antieatingactivist <antieatingactivist@gmail.com>
2023-02-16 22:43:16 -0800

    basic text box works

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+// import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [textBox, setTextBox] = useState("");
+
+  function handleSubmit() {
+    return fetch(`http://test.local/typestring?string=${textBox}`);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <input
+                value={textBox}
+                name="strip-length"
+                onChange={(e) => {setTextBox(e.target.value)}}
+                type="text"
+                placeholder=""
+                // style={inputStyle}
+        />
+        <button onClick={handleSubmit}>Send</button>
+     
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
