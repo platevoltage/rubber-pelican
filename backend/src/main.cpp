d9bfe43c9d93974040603dd55a0e3bcfbe675d27
antieatingactivist <antieatingactivist@gmail.com>
2023-02-16 18:09:01 -0800

    web server now works

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -1,22 +1,27 @@
 #include <Arduino.h>
 #include "hidkeyboard.h"
+#include "server.hpp"
 
-// #if CFG_TUD_HID
-// #define KEYBOARD_I2C_ADDR     0X5f
-// #endif
 
+#define BAUD 115200       // Any baudrate from 300 to 115200
+#define RXPIN 33         // GPIO 33 => RX for Serial1
+#define TXPIN 35         // GPIO 35 => TX for Serial1
 
 HIDkeyboard keyboard;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial1.begin(BAUD, SERIAL_8N1, RXPIN, TXPIN);
   keyboard.begin();
+  serverStart();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  keyboard.sendChar('a');
-  delay(1000);
+  webClientTimer(0);
+  yield() ;
+  // keyboard.sendChar('a');
+  // delay(1000);a
 }
 
 
---
+++ backend/src/server.hpp
@@ -0,0 +1,124 @@
+
+#include <WiFiClient.h>
+#include <WiFi.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+
+WebServer server(80);
+
+
+#define STASSID "Can't stop the signal, Mal"
+#define STAPSK "youcanttaketheskyfromme"
+#define BONJOURNAME "test"
+
+static String ssid = STASSID;
+static String password = STAPSK;
+static String bonjourName = BONJOURNAME;
+
+void handleNotFound() {
+  // digitalWrite(LED_BUILTIN, 1);
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+
+  server.send(404, "text/plain", message);
+  // digitalWrite(LED_BUILTIN, 0);
+}
+
+
+void serverStart() {
+
+    WiFi.setAutoReconnect(true);
+    WiFi.persistent(true);
+    WiFi.mode(WIFI_STA);
+    String hostname = "rubber-pelican-";
+    hostname.concat(bonjourName);
+
+    if (ssid == "") {
+
+      WiFi.hostname(hostname);
+      WiFi.softAP("rubber-pelican");
+      IPAddress myIP = WiFi.softAPIP();
+      Serial1.print("AP IP address: ");
+      Serial1.println(myIP);
+
+    } else {
+      const char* _ssid = ssid.c_str(); 
+      const char* _password = password.c_str(); 
+      WiFi.begin(_ssid, _password);
+      //   Wait for connection
+      while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial1.print(F("."));
+      }
+      while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+        Serial1.println(F("Connection Failed! Rebooting..."));
+        delay(5000);
+        ESP.restart();
+      }
+    }
+
+    Serial1.println();
+    Serial1.print(F("Connected to "));
+    Serial1.println(ssid);
+    Serial1.print(F("IP address: "));
+    Serial1.println(WiFi.localIP());
+
+    if (MDNS.begin(bonjourName.c_str())) {
+      Serial1.println(F("MDNS responder started"));
+      Serial1.println(bonjourName);
+    }
+
+
+    //-----begin generated paths
+
+
+
+
+
+    //-----end generated paths
+
+    // server.on(F("/current"), *getCurrentConfig);
+
+    server.on(F("/current"), [](){
+        Serial1.println("test");
+        server.send(200, "text/plain", "this works as well");
+    });
+
+    server.onNotFound(handleNotFound);
+    server.enableCORS(true);
+    server.begin();
+    Serial1.println(F("HTTP server started"));
+
+}
+
+
+void sendHeaders() {
+  server.sendHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  server.sendHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+  server.setContentLength(CONTENT_LENGTH_UNKNOWN);
+}
+
+
+
+
+static unsigned long webClientPreviousMillis = 0;
+void webClientTimer(uint16_t speed) {
+    unsigned long currentMillis = millis();
+    if (currentMillis - webClientPreviousMillis >= speed) {
+      webClientPreviousMillis = currentMillis;
+      server.handleClient();
+      yield();
+    }
+}
+
