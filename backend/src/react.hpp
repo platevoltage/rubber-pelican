06ae20a216041714ff8714adfbdbbe56328b788b
antieatingactivist <antieatingactivist@gmail.com>
2023-02-16 21:03:54 -0800

    react post-build up and running

---
+++ backend/src/react.hpp
@@ -0,0 +1,13 @@
+
+#include "payload/manifest.json.h"
+#include "payload/css/main.073c9b0a.css.h"
+#include "payload/js/main.3a2ae053.js.h"
+#include "payload/js/787.cda612ba.chunk.js.h"
+#include "payload.h"
+
+
+#define MANIFEST_JSON_PATH "/index.html/manifest.json"
+#define MAIN_JS_PATH "/static/css/main.073c9b0a.css"
+#define MAIN_CSS_PATH "/static/js/main.3a2ae053.js"
+#define CHUNK_JS_PATH "/static/js/787.cda612ba.chunk.js"
+#define INDEX_HTML_PATH "/index.html"
