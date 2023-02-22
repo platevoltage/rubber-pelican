fccede926d771ba956a81c9d9530d57bdb1a8216
antieatingactivist <antieatingactivist@gmail.com>
2023-02-22 15:44:02 -0800

    refactored keyboardShortcutCallback

--- backend/src/keyboard.cpp
+++ backend/src/keyboard.cpp
@@ -3,9 +3,62 @@
 
 HIDkeyboard keyboard;
 
+DuckyKeyMap duckyKeyMap[NUM_OF_KEYS] = {
+    {"COMMAND", COMMAND},
+    {"WINDOWS", WINDOWS},
+    {"GUI", GUI},
+    {"CTRL", CTRL},
+    {"OPTION", OPTION},
+    {"ALT", ALT},
+    {"SHIFT", SHIFT},
+    {"ESCAPE", ESCAPE},
+    {"ENTER", ENTER},
+    {"SPACE", SPACE},
+    {"TAB", TAB},
+    {"UPARROW", UPARROW},
+    {"DOWNARROW", DOWNARROW},
+    {"LEFTARROW", LEFTARROW},
+    {"RIGHTARROW", RIGHTARROW},
+    {"UP", UP},
+    {"DOWN", DOWN},
+    {"LEFT", LEFT},
+    {"RIGHT", RIGHT},
+    {"PAGEUP", PAGEUP},
+    {"PAGEDOWN", PAGEDOWN},
+    {"HOME", HOME},
+    {"END", END},
+    {"INSERT", INSERT},
+    {"DELETE", DELETE},
+    {"BACKSPACE", BACKSPACE},
+    {"PAUSE", PAUSE},
+    {"BREAK", BREAK},
+    {"PRINTSCREEN", PRINTSCREEN},
+    {"MENU", MENU},
+    {"APP", APP},
+    {"F1", F1},
+    {"F2", F2},
+    {"F3", F3},
+    {"F4", F4},
+    {"F5", F5},
+    {"F6", F6},
+    {"F7", F7},
+    {"F8", F8},
+    {"F9", F9},
+    {"F10", F10},
+    {"F11", F11},
+    {"F12", F12}
+};
+
 void initializeKeyboard() {
   keyboard.begin();
 }
-// void callback(String string) {  
-  // keyboard.sendString(string);
-// }
+
+int getKeycode(String keyString) {
+  for (int i = 0; i < NUM_OF_KEYS; i++) {
+    if (keyString == duckyKeyMap[i].name) {
+      return duckyKeyMap[i].code;
+    }
+  }
+  return keymap[keyString[0]].usage;
+
+}
--- backend/src/keyboard.h
+++ backend/src/keyboard.h
@@ -2,13 +2,21 @@
 #define KEYBOARD_H
 
 #include "hidkeyboard.h"
+#include "hidkeylayout.h"
+//modifier keys
 #define MOD_COMMAND 8
 #define MOD_CTRL 1
 #define MOD_OPTION 4
 #define MOD_ALT 4
 #define MOD_SHIFT 2
 
+//regular keys
+
+#define NUM_OF_KEYS 43
+
 #define COMMAND 0xE3
+#define WINDOWS 0xE3
+#define GUI 0xE3
 #define CTRL 0xE0
 #define OPTION 0xE2
 #define ALT 0xE2
@@ -16,12 +24,15 @@
 #define ESCAPE 0x29
 #define ENTER 0x28
 #define SPACE 0x2c
-
 #define TAB 0x2b
 #define UPARROW 0x52
 #define DOWNARROW 0x51
 #define LEFTARROW 0x50
 #define RIGHTARROW 0x4f
+#define UP 0x52
+#define DOWN 0x51
+#define LEFT 0x50
+#define RIGHT 0x4f
 #define PAGEUP 0x4b
 #define PAGEDOWN 0x4e
 #define HOME 0x4a
@@ -29,13 +40,11 @@
 #define INSERT 0x49
 #define DELETE 0x4c
 #define BACKSPACE 0x2a
-
 #define PAUSE 0x48
 #define BREAK 0x48
 #define PRINTSCREEN 0x46
 #define MENU 0x76
 #define APP 0x76
-
 #define F1 0x3a
 #define F2 0x3b
 #define F3 0x3c
@@ -49,11 +58,16 @@
 #define F11 0x44
 #define F12 0x45
 
-
+typedef struct {
+    String name;
+    int code;
+} DuckyKeyMap;
 
 
 extern HIDkeyboard keyboard;
 void initializeKeyboard();
 // void callback(String string); //duckyBlock uses this function to to decided what happens when a 
+
+int getKeycode(String keyString);
 
 #endif
