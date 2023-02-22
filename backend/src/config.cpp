c453ae9b2f4c0981cdbd20ff1b26f462a69eb290
antieatingactivist <antieatingactivist@gmail.com>
2023-02-22 15:59:37 -0800

    refactored keyboardShortcutCallback mods

--- backend/src/config.cpp
+++ backend/src/config.cpp
@@ -5,41 +5,9 @@ void keyboardCallback(String string) {
   Serial1.print("PRINTING TO KEYBOARD - ");
   Serial1.println(string);
 }
-      // keyboard.sendPress(0, 1); //ctrl
-      // keyboard.sendPress(0, 2); //shift
-      // keyboard.sendPress(0, 3); //ctrl/shift
-      // keyboard.sendPress(0, 4);  //option
-      // keyboard.sendPress(0, 5);   //ctrl/option
-      // keyboard.sendPress(0, 6); //option/shift
-      // keyboard.sendPress(0, 7); //option/shift/ctrl
-      // keyboard.sendPress(0, 8); //command
-      // keyboard.sendPress(0, 9); //command/ctrl
-      // keyboard.sendPress(0, 10); //command/shift
-      // keyboard.sendPress(0, 11); //command/shift/ctrl
-      // keyboard.sendPress(0, 12); //option/command
-      // keyboard.sendPress(0, 13); //option/command/ctrl
-      // keyboard.sendPress(0, 14); //command/option/shift
-      // keyboard.sendPress(0, 15); //command/shift/ctrl/option
 
 void keyboardShortcutCallback(String modifiers, String key) { 
-  // int modifierValue = 0;
-  // int keyValue = 0;
-  // bool printingKey = false;
-
-  // for (int i = 0; i < size; i++) {
-  //   if (modifiers[i].equals("OPTION")) modifierValue += MOD_OPTION;
-  //   if (modifiers[i].equals("ALT")) modifierValue += MOD_ALT;
-  //   else if (modifiers[i].equals("CTRL")) modifierValue += MOD_CTRL;
-  //   else if (modifiers[i].equals("SHIFT")) modifierValue += MOD_SHIFT;
-  //   else if (modifiers[i].equals("COMMAND")) modifierValue += MOD_COMMAND;
-  //   else if (modifiers[i].equals("WINDOWS")) modifierValue += MOD_COMMAND;
-  //   else if (modifiers[i].equals("GUI")) modifierValue += MOD_COMMAND;
-  // }
-
-  
   keyboard.sendKey(getKeyCode(key), getModCode(modifiers));
-
-
 }
 
 void delayCallback(int milliseconds) {
