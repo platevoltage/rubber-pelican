20856617bf7a728f57b1c038f4034fb81974bd75
antieatingactivist <antieatingactivist@gmail.com>
2023-02-24 18:01:48 -0800

    finally got usb devices to behave

--- backend/src/flash.cpp
+++ backend/src/flash.cpp
@@ -1,20 +1,23 @@
 #include "flash.h"
+#include "keyboard.h"
 
-FlashUSB dev;
-FlashUSB dev1;
+// FlashUSB dev;
+
 char *l1 = "ffat";
 char *l2 = "ffat1";
 bool RTC_DATA_ATTR flashActivated = false;
 
+FlashUSB dev;
+
 void initializeFlash() {
-    if (dev.init("/fat1", "ffat"))
-    {
-        if (dev.begin())
-        {
+    if (dev.init("/fat1", "ffat")) {
+        if (!flashActivated) dev.setBaseEP(3); // This is a hack that sets the ID to clash with keyboard when deactivated. Simply not initializing the flash causes the keyboard to not work.
+        if (flashActivated || keyboardActivated) {
+        if (dev.begin()) {
             Serial1.println("MSC lun 1 begin");
-            flashActivated = true;
         }
-        else
-            log_e("LUN 1 failed");
+        else Serial1.println("LUN 1 failed");
+        }
+    
     }
 }
