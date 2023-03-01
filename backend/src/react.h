a148f55070e745ec54d649ef40d212b3370822be
antieatingactivist <antieatingactivist@gmail.com>
2023-02-28 17:32:41 -0800

    updated payload

--- backend/src/react.h
+++ backend/src/react.h
@@ -1,13 +1,13 @@
 
 #include "payload/manifest.json.h"
-#include "payload/static/css/main.4f1d3d7f.css.h"
-#include "payload/static/js/main.f1d8af84.js.h"
+#include "payload/static/css/main.40869ee8.css.h"
+#include "payload/static/js/main.547672f2.js.h"
 #include "payload/static/js/787.cda612ba.chunk.js.h"
 #include "payload/index.html.h"
 
 
 #define MANIFEST_JSON_PATH "/rubber-pelican/manifest.json"
-#define MAIN_JS_PATH "/rubber-pelican/static/css/main.4f1d3d7f.css"
-#define MAIN_CSS_PATH "/rubber-pelican/static/js/main.f1d8af84.js"
+#define MAIN_JS_PATH "/rubber-pelican/static/css/main.40869ee8.css"
+#define MAIN_CSS_PATH "/rubber-pelican/static/js/main.547672f2.js"
 #define CHUNK_JS_PATH "/rubber-pelican/static/js/787.cda612ba.chunk.js"
 #define INDEX_HTML_PATH "/rubber-pelican/index.html"
